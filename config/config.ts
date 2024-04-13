3af5c50330d23a6eaa62f2ee5a78430507fc53e5
Hildorien <nachofernandez.1995@hotmail.com>
2024-04-13 13:02:10 -0300

    Update tsconfig.json, config/config.ts, and package.json

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,7 @@
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { TaskServiceConnectionType } from '../src/model/task';
 import { Config, Environment } from './types';
 import { getEnv } from './utils';
@@ -7,7 +10,7 @@ import { getEnv } from './utils';
 const environment = getEnv('NODE_ENV');
 
 if (!Object.values(Environment).includes(environment as Environment)) {
-	process.exit(1);
+    process.exit(1);
 }
 
 const connectionType = getEnv('CONNECTION_TYPE') as TaskServiceConnectionType;
@@ -16,7 +19,7 @@ const connectionString = getEnv('CONNECTION_STRING');
 let config: Config = {
     environment: environment,
     port: process.env.PORT ? Number(process.env.PORT) : 5000,
-    connection: { connectionType: connectionType , connectionString: connectionString },
+    connection: { connectionType: connectionType, connectionString: connectionString },
 }
 
 export default config;
