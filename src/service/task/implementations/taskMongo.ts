4bb09f142b3f2f266374eb8f662c7b82980939e4
Hildorien <nachofernandez.1995@hotmail.com>
2024-04-13 12:33:52 -0300

    Refactor TaskServiceMongo class in taskMongo.ts

--- src/service/task/implementations/taskMongo.ts
+++ src/service/task/implementations/taskMongo.ts
@@ -1,11 +1,10 @@
-import { TaskService } from "..";
 import { Task } from "../../../model/task";
 import { MongoClient, Collection, Db } from "mongodb";
 import { ITaskConnection } from "./taskInterface";
 
 
 export class TaskServiceMongo implements ITaskConnection {
-    
+
     private db: Db;
     private tasks: Collection<Task>;
 
@@ -13,11 +12,11 @@ export class TaskServiceMongo implements ITaskConnection {
         this.db = db;
         this.tasks = this.db.collection<Task>("tasks");
     }
-    
+
     public static async create(connectionString: string): Promise<ITaskConnection> {
         // ====================================
         // Wait for mongo service to start when running the app in a dockerize container before initilizing a connection
-        const wait = new Promise(r=>setTimeout(r,3000));
+        const wait = new Promise(r => setTimeout(r, 3000));
         await wait;
         // ====================================
         const client = await MongoClient.connect(connectionString);
@@ -32,15 +31,15 @@ export class TaskServiceMongo implements ITaskConnection {
     async getTaskById(id: number): Promise<Task> {
         return await this.tasks.findOne({ id: id });
     }
-    
+
     async createTask(newTask: Task): Promise<void> {
         await this.tasks.insertOne(newTask);
     }
-    
+
     async updateTask(updatedTask: Task): Promise<void> {
         await this.tasks.updateOne({ id: updatedTask.id }, { $set: updatedTask });
     }
-    
+
     async deleteTask(id: number): Promise<void> {
         const task = await this.tasks.findOne({ id: id });
         if (!task) {
@@ -48,9 +47,9 @@ export class TaskServiceMongo implements ITaskConnection {
         }
         await this.tasks.deleteOne({ id: id });
     }
-    
+
     async deleteAllTasks(): Promise<void> {
         await this.tasks.deleteMany({});
     }
-    
+
 }
